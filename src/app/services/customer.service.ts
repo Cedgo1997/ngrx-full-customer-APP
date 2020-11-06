13eb5956710183214dc81149eedf6d1f0bc699a9
César Gonzalez <cedgo1997@gmail.com>
2020-11-06 18:13:27 -0400

    default customer service

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+
+import { CustomerModel } from "./../models/customer.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CustomerService {
+  private customersUrl = "http://localhost:3000/customers";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCustomers(): Observable<CustomerModel[]> {
+    return this.http.get<CustomerModel[]>(this.customersUrl);
+  }
+
+  getCustomerById(id: number): Observable<CustomerModel> {
+    return this.http.get<CustomerModel>(`${this.customersUrl}/${id}`);
+  }
+
+  createCustomer(payload: CustomerModel): Observable<CustomerModel> {
+    return this.http.post<CustomerModel>(this.customersUrl, payload);
+  }
+
+  updateCustomer(customer: CustomerModel): Observable<CustomerModel> {
+    return this.http.patch<CustomerModel>(
+      `${this.customersUrl}/${customer.id}`,
+      customer
+    );
+  }
+
+  deleteCustomer(payload: number) {
+    return this.http.delete(`${this.customersUrl}/${payload}`);
+  }
 }
