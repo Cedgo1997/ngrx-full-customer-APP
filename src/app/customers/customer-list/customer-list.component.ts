fca4782994aaec300b408b13af999355a732406e
César Gonzalez <cedgo1997@gmail.com>
2020-11-06 14:19:07 -0400

    dispatch customers action

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CustomerModel } from 'src/app/models/customer.model';
 import { AppState } from './../../store/app.reducers';
+import * as customersActions from './../../store/actions/customers.actions';
 
 @Component({
   selector: 'app-customer-list',
@@ -17,5 +18,6 @@ export class CustomerListComponent implements OnInit {
     this.store
       .select('customers')
       .subscribe(({ customers }) => (this.customers = customers));
+    this.store.dispatch(customersActions.loadCustomers());
   }
 }
