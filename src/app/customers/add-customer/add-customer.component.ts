91ecf98771be7abdfdad03b2149072a6ece58d36
César Gonzalez <cedgo1997@gmail.com>
2020-11-07 23:14:44 -0400

    Reactive forms added to customer ts

--- src/app/customers/add-customer/add-customer.component.ts
+++ src/app/customers/add-customer/add-customer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-customer',
   templateUrl: './add-customer.component.html',
-  styleUrls: ['./add-customer.component.css']
+  styleUrls: ['./add-customer.component.css'],
 })
 export class AddCustomerComponent implements OnInit {
+  customerForm: FormGroup;
 
-  constructor() { }
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-  }
 
+    this.customerForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+      membership: ['', Validators.required]
+    })
+
+
+  }
 }
